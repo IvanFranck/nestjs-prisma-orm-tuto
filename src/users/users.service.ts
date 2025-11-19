7a6d6827a177ae3af603ad7af95bcae12134adc3
NZIMA Ivan <nzimaivan007@gmail.com>
2025-11-19 04:18:31 +0100

    Add @nestjs/config dependency, configure global settings in AppModule, and implement Users module with controller and service.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,4 @@
-
 import { Injectable } from '@nestjs/common';
-import { User } from 'generated/prisma/client';
-import { PrismaService } from 'src/prisma.service';
-
 
 @Injectable()
-export class UsersService {
-  constructor(private prisma: PrismaService) {}
-
-  async user(
-    id: number,
-  ): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where: {id},
-      include: {
-        posts: true
-      }
-    });
-  }
-
-
-}
+export class UsersService {}
